23f55eb288c39934c5189444d03cc1ce2ce5a3d4
Jayloneal <121124684+Jayloneal@users.noreply.github.com>
2023-04-09 08:57:39 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <ul>
+    <h1>Favorite People!</h1>
+    <li>My mother!</li>
+    <li>My father!</li>
+    <li>My siblings!</li>
+  </ul>,
+  document.getElementById("root")
+);
